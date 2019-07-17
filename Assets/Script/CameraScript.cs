d2c92fec8a410871122976eb3951da4c00b83d1c
cchaiyatad <chaiyatad@gmail.com>
2019-07-17 13:12:42 +0700

    Finish Menu

---
+++ Assets/Script/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform Player;
+    private Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Offset = Player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position - Offset;
+    }
+}
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed = 2f;
+    private Rigidbody rigidbody;
+            
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move(new Vector3(Input.GetAxis("Horizontal"),0 ,Input.GetAxis("Vertical")));
+    }
+
+    void Move(Vector3 direction){
+        rigidbody.AddForce(direction * speed);
+    }
+}
