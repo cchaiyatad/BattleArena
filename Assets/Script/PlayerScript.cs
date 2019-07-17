9e4b24ea66d09390d8548ef8f97c05a1f0d686fd
cchaiyatad <chaiyatad@gmail.com>
2019-07-17 15:12:01 +0700

    Make character moveable

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float speed = 2f;
-    private Rigidbody rigidbody;
-            
+    public float speed = 0.1f;
+
+    private Vector3 direction;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         
+        if (direction != Vector3.zero)
+        {
+            animator.SetBool("IsMove", true);
+        }
+        else
+        {
+            animator.SetBool("IsMove",  false);
+        }
     }
 
     void FixedUpdate()
     {
-        Move(new Vector3(Input.GetAxis("Horizontal"),0 ,Input.GetAxis("Vertical")));
+        Move(direction);
+
     }
 
-    void Move(Vector3 direction){
-        rigidbody.AddForce(direction * speed);
+    void Move(Vector3 dir){
+        if (dir == Vector3.zero) {
+            return;
+        }
+        transform.Translate(dir * speed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, Mathf.Atan(dir.x / dir.z) * 180 / Mathf.PI, 0)
+            * Time.fixedDeltaTime,Space.World );
+
+
     }
+
+
 }
